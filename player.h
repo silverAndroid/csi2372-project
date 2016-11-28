d4ab922bf31f2c5158b17cbb570d564a0cabe690
Brandon Danis <brandon_danis@hotmail.com>
2016-11-28 16:35:11 -0500

    Renamed player.h and added functions to header

---
+++ player.h
@@ -0,0 +1,25 @@
+//
+// Created by Brandon Danis on 2016-11-24.
+//
+
+#ifndef CSI2372_PROJECT_PLAYERS_H
+#define CSI2372_PROJECT_PLAYERS_H
+#include <string>
+
+class Player {
+    std::string playerName;
+    int numOfCoins;
+    int maxNumOfChains = 2;
+public:
+    Player( std::string &);
+    std::string getName();
+    int getNumCoins();
+    Player& operator+=(int);
+    int getMaxNumChains();
+    int getNumChains();
+    void buyThirdChain();
+    void printHand(std::ostream&, bool);
+};
+
+
+#endif //CSI2372_PROJECT_PLAYERS_H
