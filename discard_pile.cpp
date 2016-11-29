488e4767b75e19a02a8df48147cbc26fed7cdb4a
Rushil Perera <pererarushil1940@gmail.com>
2016-11-29 14:31:57 -0500

    Implemented DiscardPile

---
+++ discard_pile.cpp
@@ -0,0 +1,37 @@
+//
+// Created by silver_android on 29/11/16.
+//
+
+#include "discard_pile.h"
+
+DiscardPile::DiscardPile(std::istream &, CardFactory *) {
+
+}
+
+DiscardPile &DiscardPile::operator+=(Card *card) {
+    push_back(card); // TODO: Check if any side effects from using same memory location
+    return *this;
+}
+
+Card *DiscardPile::pickUp() {
+    Card *top = new Ruby();
+    *top = *this->top();
+    pop_back();
+    return top;
+}
+
+Card *DiscardPile::top() {
+    Card *top = back();
+    return top;
+}
+
+void DiscardPile::print(std::ostream &output) {
+    for (int i = 0; i < size(); ++i) {
+        output << *operator[]((unsigned long) i);
+    }
+}
+
+std::ostream &operator<<(std::ostream &output, const DiscardPile &discardPile) {
+    output << *discardPile.back();
+    return output;
+}
---
+++ discard_pile.h
@@ -0,0 +1,23 @@
+//
+// Created by silver_android on 29/11/16.
+//
+
+#ifndef CSI2372_PROJECT_DISCARDPILE_H
+#define CSI2372_PROJECT_DISCARDPILE_H
+
+
+#include <vector>
+#include "gemstones.h"
+#include "cardFactory.h"
+
+class DiscardPile : std::vector<Card*> {
+    DiscardPile&operator+=(Card *);
+    Card *pickUp();
+    Card *top();
+    void print(std::ostream &);
+    friend std::ostream& operator<<(std::ostream &, const DiscardPile &);
+    DiscardPile(std::istream &, CardFactory *);
+};
+
+
+#endif //CSI2372_PROJECT_DISCARDPILE_H
