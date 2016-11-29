939b7117827130d8e34bb9aed38226309fcf4b6c
Rushil Perera <pererarushil1940@gmail.com>
2016-11-29 15:37:03 -0500

    Implemented TradeArea

---
+++ trade_area.cpp
@@ -0,0 +1,50 @@
+//
+// Created by silver_android on 29/11/16.
+//
+
+#include "trade_area.h"
+
+TradeArea::TradeArea(std::istream &, CardFactory *) {
+
+}
+
+TradeArea &TradeArea::operator+=(Card *card) {
+    push_back(card); // TODO: Check for any side-effects from using same memory location
+    return *this;
+}
+
+bool TradeArea::legal(Card *card) {
+    std::list<Card*>::const_iterator it = begin();
+    while (it != end()) {
+        Card *card1 = *it;
+        if (card->getName() == card1->getName()) {
+            return true;
+        }
+    }
+    return false;
+}
+
+Card *TradeArea::trade(string cardName) {
+    std::list<Card*>::const_iterator it = begin();
+    while (it != end()) {
+        Card *card = *it; // TODO: Check if any side effects from using same memory location
+        if (cardName == card->getName()) {
+            remove(card);
+            return card;
+        }
+    }
+    return nullptr;
+}
+
+int TradeArea::numCards() {
+    return (int) size();
+}
+
+std::ostream &operator<<(std::ostream &output, const TradeArea &tradeArea) {
+    std::list<Card *>::const_iterator it = tradeArea.begin();
+    while (it != tradeArea.end()) {
+        Card card = **it;
+        output << card;
+    }
+    return output;
+}
---
+++ trade_area.h
@@ -0,0 +1,23 @@
+//
+// Created by silver_android on 29/11/16.
+//
+
+#ifndef CSI2372_PROJECT_TRADEAREA_H
+#define CSI2372_PROJECT_TRADEAREA_H
+
+#include <list>
+#include "gemstones.h"
+#include "cardFactory.h"
+
+class TradeArea : std::list<Card*> {
+public:
+    TradeArea& operator+=(Card *);
+    bool legal(Card *);
+    Card *trade(string);
+    int numCards();
+    friend std::ostream& operator<<(std::ostream &, const TradeArea &);
+    TradeArea(std::istream &, CardFactory *);
+};
+
+
+#endif //CSI2372_PROJECT_TRADEAREA_H
