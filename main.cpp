117d8c927e5565e91be04dfd89bfb85bfcefc124
Brandon Danis <brandon_danis@hotmail.com>
2016-12-01 20:22:25 -0500

    starting to setup main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
+#include <string>
+#include "player.h"
+#include "deck.h"
+#include "cardFactory.h"
 
 int main() {
+
+    bool loadLastGame = false;
+
+    Player* player1;
+    Player* player2;
+
+    std::string player1Name("Brandon");
+    std::string player2Name("Rushil");
+
+    //#####################//
+    //####### SETUP #######//
+    //#####################//
+
+    if(loadLastGame){
+        //load players from last game
+    }else{
+        player1 = new Player(player1Name);
+        player2 = new Player(player2Name);
+    }
+
+
     return 0;
 }
