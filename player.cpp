1bdb837c0078ba1b812604e2e21fe1765f0958c2
Brandon Danis <brandon_danis@hotmail.com>
2016-12-01 19:46:46 -0500

    Added comments to player class

--- player.cpp
+++ player.cpp
@@ -4,6 +4,7 @@
 
 #include "player.h"
 
+// constructor that creates a Player with a given name
 Player::Player( std::string& name ){
     maxNumOfChains = 2;
     numOfChains = 0;
@@ -12,31 +13,40 @@ Player::Player( std::string& name ){
     chains[1] = Chain_Base();
 }
 
+// constructor which accepts an std::istream and reconstruct the Player from file.
 Player::Player(std::istream&, CardFactory*) {
 
 }
 
+// get the name of the player
 std::string Player::getName() {
     return playerName;
 }
 
+// get the number of coins currently held by the player
 int Player::getNumCoins() {
     return numOfCoins;
 }
 
+// add a number of coins
 Player& Player::operator+=(int x) {
     numOfCoins = numOfCoins + x;
     return *this;
 }
 
+// returns either 2 or 3.
 int Player::getMaxNumChains() {
     return maxNumOfChains;
 }
 
+// returns the number of non-zero chains
 int Player::getNumChains() {
     return numOfChains;
 }
 
+/*  adds an empty third chain to the player for two coins. The functions
+reduces the coin count for the player by two. If the player does not have enough coins then an exception
+NotEnoughCoins is thrown */
 void Player::buyThirdChain(){
     if(numOfCoins < 2) {
         //throw exception
@@ -49,10 +59,13 @@ void Player::buyThirdChain(){
     chains[2] = Chain_Base();
 }
 
+// returns the chain at position i.
 Chain_Base& Player::operator[](int i) {
     return chains[i];
 }
 
+/* prints the top card of the player's hand (with
+argument false) or all of the player's hand (with argument true) to the supplied std::ostream */
 //void Player::printHand(std::ostream &output, bool b) {
 //    if(b){
 //        //output << hand.top();
