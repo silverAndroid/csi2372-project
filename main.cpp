542d59e04b379d21ef43cd3907f85b18819e3fa0
Brandon Danis <bdani093@uottawa.ca>
2016-12-08 16:02:21 -0500

    Added pseudo code to main loop

--- main.cpp
+++ main.cpp
@@ -87,10 +87,38 @@ int main() {
                     Chain_Base tempChain = (*currentPlayer)[i];
                     auto tempType = tempChain.getCardType();
 
-
-
                 }
             }
+
+            //Play topmost card from Hand.
+
+            //If chain is ended, cards for chain are removed and player receives coin(s).
+
+            //If player decides to
+                    //Play the now topmost card from Hand.
+
+            //If chain is ended, cards for chain are removed and player receives coin(s).
+
+            //If player decides to
+                    //Show the player's full hand and player selects an arbitrary card
+                    //Discard the arbitrary card from the player's hand and place it on the discard pile.
+
+            //Draw three cards from the deck and place cards in the trade area
+
+            //while top card of discard pile matches an existing card in the trade area
+                    //draw the top-most card from the discard pile and place it in the trade area
+            //end
+
+            //for all cards in the trade area
+                //if player wants to chain the card
+                    //chain the card
+                    //If chain is ended
+                        //cards for chain are removed and player receives coin(s).
+                //else
+                    //card remains in trade area for the next player.
+            //end
+
+            //Draw two cards from Deck and add the cards to the player's hand (at the back)
 
         }
 
