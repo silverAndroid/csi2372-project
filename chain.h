1026c49ee75c8025b2ab5855976aecebbb037565
Rushil Perera <pererarushil1940@gmail.com>
2016-12-08 01:31:41 -0500

    Merged chain header and cpp file into header

--- chain.h
+++ chain.h
@@ -6,6 +6,7 @@
 #define CSI2372_PROJECT_CHAIN_H
 
 #include <vector>
+#include <typeinfo>
 #include "gemstones.h"
 #include "cardFactory.h"
 
@@ -17,13 +18,46 @@ template <typename T>
 class Chain : Chain_Base {
     std::vector<T*> cards;
 public:
-    Chain();
-    Chain(std::istream &, CardFactory *);
-    Chain<T>&operator+=(Card *);
-    T getCardType();
-    int sell();
-    friend std::ostream& operator<<(std::ostream &, const Chain<T> &);
+    Chain(std::istream &, CardFactory *) {
+
+    }
+
+    Chain() {
+
+    }
+
+    Chain &operator+=(Card *card) {
+        T *t;
+        if ((t = (T *) (dynamic_cast<T *>(card) == nullptr))) {
+            //TODO: Need to run test to make sure it works and uses the right types
+            throw IllegalTypeException(card->getName(), typeid(T).name());
+        } else {
+            cards.push_back(t);
+        }
+        return *this;
+    }
+
+    T getCardType() {
+        return typeid(T).name();
+    }
+
+    int sell() {
+        Card *card = cards[0];
+        return card->getCardsPerCoin((int) cards.size());
+    }
+
+    friend std::ostream &operator<<(std::ostream &output, const Chain &chain);
 };
+
+template <typename T>
+std::ostream &operator<<(std::ostream &output, const Chain<T> &chain) {
+    output << chain.cards[0].getName();
+    output << string("\t");
+    for (int i = 0; i < chain.cards.size(); ++i) {
+        output << chain.cards[i];
+    }
+    return output;
+}
 
 
 #endif //CSI2372_PROJECT_CHAIN_H
