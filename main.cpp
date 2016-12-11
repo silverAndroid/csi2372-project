964c42e4cf500fa61d0616cfa8c3cf334d82bb5f
Rushil Perera <pererarushil1940@gmail.com>
2016-12-11 15:32:12 -0500

    Implemented pause and save, removed TODOs, separated dyanmic cast Chain logic, changed tempType and cardType to string and more..
    Added removed chains to discard pile, and added method to display big messages

--- main.cpp
+++ main.cpp
@@ -1,54 +1,30 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include "player.h"
 #include "table.h"
 #include "cardFactory.h"
 
+Chain_Base *getChain(string cardType) ;
+
+void displayMessage(std::string message) ;
+
 using std::cout;
 using std::cin;
 using std::endl;
 
-void displayMessage(std::string message) {
-	int length = message.size() + 18;
-	string topLine = "";
-	string middleLine = "";
-	string messageLine = "";
-	const string eightSpaces = "        ";
-	std::transform(message.begin(), message.end(), message.begin(), ::toupper);
-
-	for (size_t i = 0; i < length; i++)
-	{
-		topLine += "=";
-	}
-
-	for (size_t i = 0; i < length; i++)
-	{
-		bool isEqualSign = i == 0 || i == length - 1;
-		middleLine += isEqualSign ? "=" : " ";
-	}
-
-	messageLine += "=";
-	messageLine += eightSpaces;
-	messageLine += message;
-	messageLine += eightSpaces; 
-	messageLine += "=";
-
-	cout << endl;
-	cout << topLine << endl;
-	cout << middleLine << endl;
-	cout << messageLine << endl;
-	cout << middleLine << endl;
-	cout << topLine << endl;
-	cout << endl;
-}
-
 int main() {
+
+	//TODO: Do some error validation
 
 	//////////////////
 	/// GAME SETUP ///
 	//////////////////
 
-	bool loadLastGame = false;
+	std::ifstream saveData("gameData.sav");
+	bool loadLastGame = saveData.good();
+	saveData.close();
+	bool firstLoop = true;
 
 	std::string player1Name = "Brandon";
 	std::string player2Name = "Rushil";
@@ -85,7 +61,6 @@ int main() {
 		gameDiscardPile = new DiscardPile();
 
 		gameTable = new Table(player1, player2, &gameDeck, gameTradeArea, gameDiscardPile);
-
 	}
 
 	/////////////////
@@ -98,20 +73,32 @@ int main() {
 	while (!gameTable->win(winningName)) {
 
 		//if pause save game to file and exit
-		//TODO: Implement saving entire game
+		if (!firstLoop) {
+			cout << "Would you like to save and exit the game? ";
+			cin >> response;
+			if (response == 'Y') {
+				std::ofstream saveFile;
+				saveFile.open("gameData.sav");
+				saveFile << *gameTable;
+				saveFile << "Deck: " << gameDeck;
+				saveFile.close();
+				return 0;
+			}
+		}
+
+		firstLoop = false;
 
 		//For each Player
 		for (Player* currentPlayer : { player1,player2 }) {
 			displayMessage(currentPlayer->getName() + "\'s turn");
 			Hand *currentHand = currentPlayer->getHand();
+			bool cardPlayed = false;
 
 			//Display Table
-			//TODO: Display table
 			cout << *gameTable << endl;
 
 			//If Player has 3 coins and two chains and decides to buy extra chain
 			if (currentPlayer->getNumCoins() > 2 && currentPlayer->getMaxNumChains() == 2) {
-				//TODO: Offer to buy chain
 				cout << "Would you like to buy a third chain? 'Y' for yes" << endl;
 				cin >> response;
 
@@ -127,17 +114,62 @@ int main() {
 			currentPlayer->addCardToHand(gameDeck.draw());
 
 			//If TradeArea is not empty
-			if (!gameTradeArea->isEmpty()) {
+			while (!gameTradeArea->isEmpty()) {
 				//Add gemstone cards from the TradeArea to chains or discard them.
-				for (int i = 0; i < currentPlayer->getNumChains(); ++i) {
-					Chain_Base tempChain = (*currentPlayer)[i];
-					auto tempType = tempChain.getCardType();
+				cout << "Trade Area: " << *gameTradeArea << std::endl;
+				cout << "Would you like to add a card from the trade area to a chain? Any remaining cards will be sent to the discard pile. 'Y' for yes" << endl;
+				cin >> response;
+				if (response != 'Y') {
+					response = 0;
+					break;
+				}
+				response = 0;
 
+				cout << "Which card do you want to chain? (Enter index)" << endl;
+				int index = 0;
+				cin >> index; 
+				cardPlayed = false;
+				Card *tradedCard = gameTradeArea->trade(index - 1);
+
+				for (int i = 0; i < currentPlayer->getNumChains(); ++i) {
+					Chain_Base *tempChain = &((*currentPlayer)[i]);
+					string tempType = tempChain->getCardType();
+					string cardType = tradedCard->getName();
+					if (tempType == cardType) {
+                        tempChain = getChain(cardType);
+						cardPlayed = true;
+						break;
+					}
+				}
+
+				if (!cardPlayed) {
+					if (currentPlayer->getMaxNumChains() == currentPlayer->getNumChains()) {
+						index = 0;
+						cout
+							<< "You have reached the maximum amount of chains allowed. Which chain would you like to remove?"
+							<< endl;
+						for (size_t i = 0; i < currentPlayer->getNumChains(); i++) {
+							cout << "Chain " << (i + 1) << ": ";
+							cout << currentPlayer->operator[](i) << endl;
+						}
+						cin >> index;
+						int coinsAdded = 0;
+						Chain_Base *removedChain = currentPlayer->removeChain(index - 1, coinsAdded);
+						//If chain is ended, cards for chain are removed and player receives coin(s).
+						cout << "Sold for " << coinsAdded << " coins" << endl;
+					}
+					currentPlayer->createNewChain(tradedCard);
+					cardPlayed = true;
 				}
 			}
 
+			//Discarding all cards in trade area
+			while (!gameTradeArea->isEmpty()) {
+				*gameDiscardPile += gameTradeArea->trade(0);
+			}
+
 			//Play topmost card from Hand.
-			bool cardPlayed = false;
+			cardPlayed = false;
 
 			for (int k = 0; k < 2; ++k) {
 
@@ -155,34 +187,10 @@ int main() {
 
 				for (int i = 0; i < currentPlayer->getNumChains(); ++i) {
 					Chain_Base *tempChain = &((*currentPlayer)[i]);
-					auto tempType = tempChain->getCardType();
-					auto cardType = currentHand->top()->getName();
+					string tempType = tempChain->getCardType();
+					string cardType = currentHand->top()->getName();
 					if (tempType == cardType) {
-						cout << "Found a matching chain for " << cardType << endl;
-						if (cardType == "Quartz") {
-							*dynamic_cast<Chain<Quartz> *>(tempChain) += currentHand->play();
-						}
-						else if (cardType == "Hematite") {
-							*dynamic_cast<Chain<Hematite> *>(tempChain) += currentHand->play();
-						}
-						else if (cardType == "Obsidian") {
-							*dynamic_cast<Chain<Obsidian> *>(tempChain) += currentHand->play();
-						}
-						else if (cardType == "Malachite") {
-							*dynamic_cast<Chain<Malachite> *>(tempChain) += currentHand->play();
-						}
-						else if (cardType == "Turquoise") {
-							*dynamic_cast<Chain<Turquoise> *>(tempChain) += currentHand->play();
-						}
-						else if (cardType == "Ruby") {
-							*dynamic_cast<Chain<Ruby> *>(tempChain) += currentHand->play();
-						}
-						else if (cardType == "Amethyst") {
-							*dynamic_cast<Chain<Amethyst> *>(tempChain) += currentHand->play();
-						}
-						else if (cardType == "Emerald") {
-							*dynamic_cast<Chain<Emerald> *>(tempChain) += currentHand->play();
-						}
+						tempChain = getChain(cardType);
 						cardPlayed = true;
 						break;
 					}
@@ -199,7 +207,8 @@ int main() {
 							cout << currentPlayer->operator[](i) << endl;
 						}
 						cin >> index;
-						int coinsAdded = currentPlayer->removeChain(index - 1);
+						int coinsAdded = 0;
+						Chain_Base *removedChain = currentPlayer->removeChain(index - 1, coinsAdded);
 						//If chain is ended, cards for chain are removed and player receives coin(s).
 						cout << "Sold for " << coinsAdded << " coins" << endl;
 					}
@@ -223,16 +232,17 @@ int main() {
 			if (response == 'Y') {
 				cout << "Hand: " << *currentHand << endl;
 				int index = 0;
-				cout << "Please select a card to go into the discard pile (Enter index): ";
+				cout << "Which card do you want to discard? (Enter index) " << endl;
 				cin >> index;
 				Card *discardedCard = (*currentHand)[index - 1];
 				*gameDiscardPile += discardedCard;
 			}
 
 			//Draw three cards from the deck and place cards in the trade area
-			*gameTradeArea += gameDeck.draw();
-			*gameTradeArea += gameDeck.draw();
-			*gameTradeArea += gameDeck.draw();
+			for (size_t i = 0; i < 3; i++)
+			{
+				*gameTradeArea += gameDeck.draw();
+			}
 
 			//while top card of discard pile matches an existing card in the trade area
 			while (!gameDiscardPile->isEmpty() && gameTradeArea->legal(gameDiscardPile->top())) {
@@ -266,35 +276,11 @@ int main() {
 				Card *tradedCard = gameTradeArea->trade(index - 1);
 
 				for (int i = 0; i < currentPlayer->getNumChains(); ++i) {
-					Chain_Base *tempChain = &((*currentPlayer)[i]);
-					auto tempType = tempChain->getCardType();
-					auto cardType = tradedCard->getName();
+					Chain_Base *tempChain = &(currentPlayer->operator[](i));
+					string tempType = tempChain->getCardType();
+					string cardType = tradedCard->getName();
 					if (tempType == cardType) {
-						cout << "Found a matching chain for " << cardType << endl;
-						if (cardType == "Quartz") {
-							*dynamic_cast<Chain<Quartz> *>(tempChain) += tradedCard;
-						}
-						else if (cardType == "Hematite") {
-							*dynamic_cast<Chain<Hematite> *>(tempChain) += tradedCard;
-						}
-						else if (cardType == "Obsidian") {
-							*dynamic_cast<Chain<Obsidian> *>(tempChain) += tradedCard;
-						}
-						else if (cardType == "Malachite") {
-							*dynamic_cast<Chain<Malachite> *>(tempChain) += tradedCard;
-						}
-						else if (cardType == "Turquoise") {
-							*dynamic_cast<Chain<Turquoise> *>(tempChain) += tradedCard;
-						}
-						else if (cardType == "Ruby") {
-							*dynamic_cast<Chain<Ruby> *>(tempChain) += tradedCard;
-						}
-						else if (cardType == "Amethyst") {
-							*dynamic_cast<Chain<Amethyst> *>(tempChain) += tradedCard;
-						}
-						else if (cardType == "Emerald") {
-							*dynamic_cast<Chain<Emerald> *>(tempChain) += tradedCard;
-						}
+                        tempChain = getChain(cardType);
 						cardPlayed = true;
 						break;
 					}
@@ -311,7 +297,8 @@ int main() {
 							cout << currentPlayer->operator[](i) << endl;
 						}
 						cin >> index;
-						int coinsAdded = currentPlayer->removeChain(index - 1);
+						int coinsAdded = 0;
+						Chain_Base *removedChain = currentPlayer->removeChain(index - 1, coinsAdded);
 						//If chain is ended, cards for chain are removed and player receives coin(s).
 						cout << "Sold for " << coinsAdded << " coins" << endl;
 					}
@@ -333,4 +320,70 @@ int main() {
 
 	cout << winningName << " has won!" << endl;
 	return 0;
+}
+
+Chain_Base *getChain(string cardType) {
+	Chain_Base *tempChain = new Chain_Base();
+	cout << "Found a matching chain for " << cardType << endl;
+	if (cardType == "Quartz") {
+		dynamic_cast<Chain<Quartz> *>(tempChain);
+	}
+	else if (cardType == "Hematite") {
+		dynamic_cast<Chain<Hematite> *>(tempChain);
+	}
+	else if (cardType == "Obsidian") {
+		dynamic_cast<Chain<Obsidian> *>(tempChain);
+	}
+	else if (cardType == "Malachite") {
+		dynamic_cast<Chain<Malachite> *>(tempChain);
+	}
+	else if (cardType == "Turquoise") {
+		dynamic_cast<Chain<Turquoise> *>(tempChain);
+	}
+	else if (cardType == "Ruby") {
+		dynamic_cast<Chain<Ruby> *>(tempChain);
+	}
+	else if (cardType == "Amethyst") {
+		dynamic_cast<Chain<Amethyst> *>(tempChain);
+	}
+	else if (cardType == "Emerald") {
+		dynamic_cast<Chain<Emerald> *>(tempChain);
+	} else {
+        throw IllegalTypeException(cardType, "Quartz, Hematite, Obsidian, Malachite, Turquoise, Ruby, Amethyst, or Emerald");
+    }
+    return tempChain;
+}
+
+void displayMessage(std::string message) {
+	int length = message.size() + 18;
+	string topLine = "";
+	string middleLine = "";
+	string messageLine = "";
+	const string eightSpaces = "        ";
+	std::transform(message.begin(), message.end(), message.begin(), ::toupper);
+
+	for (size_t i = 0; i < length; i++)
+	{
+		topLine += "=";
+	}
+
+	for (size_t i = 0; i < length; i++)
+	{
+		bool isEqualSign = i == 0 || i == length - 1;
+		middleLine += isEqualSign ? "=" : " ";
+	}
+
+	messageLine += "=";
+	messageLine += eightSpaces;
+	messageLine += message;
+	messageLine += eightSpaces;
+	messageLine += "=";
+
+	cout << endl;
+	cout << topLine << endl;
+	cout << middleLine << endl;
+	cout << messageLine << endl;
+	cout << middleLine << endl;
+	cout << topLine << endl;
+	cout << endl;
 }
